188b09d7263f9d4b05cb4fd9870c81b38d911bf1
Max <mlevinson1@harvard.mgh.edu>
2018-01-18 15:06:36 -0500

    Added Tests Suite

---
+++ tests.py
@@ -0,0 +1,12 @@
+import os
+import unittest
+import *
+
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        
+    def tearDown(self):
+
+if __name__ == '__main__':
+    unittest.main()
