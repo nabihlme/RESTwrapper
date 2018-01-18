61c81facb0a0aee3d85c497ed4695939f5979d5c
Max <mlevinson1@harvard.mgh.edu>
2018-01-18 15:10:19 -0500

    Response objects added for returning common errors

---
+++ apiResponses.py
@@ -0,0 +1,33 @@
+from flask import Response as flaskResponse
+import requests
+
+def buildResponse(ReqResponse):
+    '''Transfer requests response object into a flask Response object'''
+    builtResponse = flaskResponse(
+            status = ReqResponse.status_code,
+            response = ReqResponse.content, 
+            mimetype = 'text/plain'
+            )
+    return builtResponse 
+
+# 401 Unauthorized
+EmptyAuth = flaskResponse(
+        response = 'Missing Login information, cannot use MDS without authentication',
+        status = 401,
+        mimetype = 'text/plain'
+        )
+
+#Create some different error response objects for flask
+PostGet405 = flaskResponse( 
+        response = 'Method Not Allowed: POST or GET at this Endpoint', 
+        status = 405,
+        mimetype = 'text/plain'
+        )
+
+Get405 = flaskResponse( 
+        response = 'Method Not Allowed: Only GET at this Endpoint', 
+        status = 405,
+        mimetype = 'text/plain'
+        )
+
+
--- tests.py
+++ tests.py
@@ -1,12 +1,22 @@
 import os
 import unittest
-import *
+from app import *
 
 class FlaskTestCase(unittest.TestCase):
 
+    # set up and tear down
     def setUp(self):
-        
+        app.config['Testing'] = True
+        app.config['Debug'] = True
+        self.app = app.test_client()
+
     def tearDown(self):
+        pass
+
+    # List of Tests
+    def test_main(self):
+        response = self.app.get('/doi')
+        self.assertEqual(response.status, 401)
 
 if __name__ == '__main__':
     unittest.main()
